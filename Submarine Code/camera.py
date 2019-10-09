e9e1a3e5a41c4b5a517a397b1d6a1aadff951429
Aaron Smith <porousindiana7@gmail.com>
2019-10-09 09:26:11 -0400

    first upload of files

---
+++ Submarine Code/camera.py
@@ -0,0 +1,39 @@
+import sys
+from picamera import PiCamera
+from tkinter import *
+from tkinter.ttk import *  
+
+
+# Initialize tkinter window with dimensions 100x100               
+
+# create a tkinter window 
+root = Tk()    
+
+root.geometry('200x400+1200+300')   
+
+
+camera = PiCamera()
+
+camera.start_preview(fullscreen=False, window = (0, 400, 1000, 720))
+
+# camera.zoom=(x/100.,x/100.,0.5,0.5)
+
+
+
+def quitHandler():
+    print ("Goodbye")
+    root.destroy()
+    camera.close()
+
+
+def callback1():
+    print ("end program button clicked")
+
+
+btn = Button(root, text = 'End Program', width=200, command = quitHandler)
+
+btn.pack(side = 'top')
+
+root.mainloop()
+
+
