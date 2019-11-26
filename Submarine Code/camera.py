b3fb1e8c4276797c7d2278b08b5300b88f0d8cb7
porousindiana7 <37353301+porousindiana7@users.noreply.github.com>
2019-11-26 10:58:25 -0500

    Update camera.py

--- Submarine Code/camera.py
+++ Submarine Code/camera.py
@@ -1,35 +1,14 @@
-import sys
-from picamera import PiCamera
-from tkinter import *
-from tkinter.ttk import *
+import time
+import picamera
+import os
+#os.system('mpg321 B6GD.mp3 &')
 
 
-# Initialize tkinter window with dimensions 100x100
-
-# create a tkinter window
-root = Tk()
-
-root.geometry('200x400+1200+300')
-
-
-camera = PiCamera()
-
-camera.start_preview(fullscreen=False, window = (0, 400, 1000, 720))
-
-# camera.zoom=(x/100.,x/100.,0.5,0.5)
-
-def quitHandler():
-    print ("Goodbye")
-    root.destroy()
+camera = picamera.PiCamera()
+try:
+    camera.start_preview()
+    time.sleep(60)
+    camera.stop_preview()
+finally:
     camera.close()
-
-
-def callback1():
-    print ("end program button clicked")
-
-
-btn = Button(root, text = 'End Program', width=200, command = quitHandler)
-
-btn.pack(side = 'top')
-
-root.mainloop()
+  
