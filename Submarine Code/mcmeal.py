46614f9b7f0fc953d9f96dcb1885026a8ef7200b
porousindiana7 <37353301+porousindiana7@users.noreply.github.com>
2019-12-04 10:11:41 -0500

    Update mcmeal.py

--- Submarine Code/mcmeal.py
+++ Submarine Code/mcmeal.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO          
 from time import sleep
+import keyboard
+import time
 
 in1 = 24
 in2 = 23
@@ -19,64 +21,28 @@ print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
-while(1):
-
-    x=input()
-    
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         print("forward")
-         x='z'
+while True:  # making a loop
+    try:
+        #Slow Forward
+        if keyboard.is_pressed("w"):  # if key 'w' is pressed 
+            print("\n forward (w)")
+            p.ChangeDutyCycle(100)
+            GPIO.output(in1,GPIO.HIGH)
+            GPIO.output(in2,GPIO.LOW)
+        
+        #Slow Back
+        elif keyboard.is_pressed("s"):
+            print("\n backward (s)")
+            p.ChangeDutyCycle(100)
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.HIGH)      
+        
+        #Stop
         else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         print("backward")
-         x='z'
-
-
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        x='z'
-
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        temp1=1
-        x='z'
-
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.LOW)
+            print("\n no input")   
+    except:
+        print("shouldn't see this")
+        #break  # if user pressed a key other than the given key,  break
+    time.sleep(0.05)
